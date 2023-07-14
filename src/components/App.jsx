c707749652a3339f7986b1563ab2acbd400612a2
Serhii Ozeran <ozeransm@gmail.com>
2023-07-15 00:08:12 +0300

    task 3, update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useReducer } from "react";
 import { nanoid } from 'nanoid'
 import { getImage } from "./Api";
 import { ImageGallery } from "./ImageGallery";
@@ -7,86 +7,109 @@ import css from '../Styles.module.css';
 import { Modal } from "./Modal";
 import { Loader } from "./Loader";
 import { Searchbar } from "./Searchbar";
-import { LoadMore } from "./Button";
+import { LoadMore } from "./LoadMore";
 const spinner = document.getElementById('spinner');
 const modal = document.getElementById('modal');
 
-class App extends Component {
-  state={
+function reducer(state, action) {
+  
+  switch (Object.keys(action)[0]) {
+    case 'query':
+      return {...state, ...{isQuery: action.query}};
+    case 'objImg':
+      return {...state, ...action.objImg};
+    case 'isModalOpen': 
+      return {...state, ...{isModalOpen: action.isModalOpen}};
+    case 'isLoading':
+      return {...state, ...{isLoading: action.isLoading}};
+    case 'error':
+      return {...state, ...{error: action.error}};
+    case 'page':
+      return {...state, ...{page: action.page}};
+    default: break;
+      
+  }
+}
+const initialState={
     objImg: [],
     isLoading: false,
     isModalOpen: 0,
     isQuery: '',
     error: null,
     page: 1,
-    totalPage: 0
-  }
-  
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.isQuery && prevState.isQuery!==this.state.isQuery){
-    this.setState({isLoading: true});
-    setTimeout(()=>{
+    totalPage: 0,
     
-    try{
-      getImage(this.state.isQuery, 1)
-      .then(({data})=>{
-        const objImg = data.hits.map((el)=>({id: nanoid(), webformatURL: el.webformatURL, largeImageURL: el.largeImageURL}));
-        this.setState({objImg, totalPage: data.totalHits, isLoading: false, page: 1});
-      });
-    }catch(error){
-      console.log(error);
-      this.setState({error});
-    }
-    
-  },1000);
-  }
-  }
-
-  handlerOpenImg=(id)=>{
-    this.setState({isModalOpen: id})
-  }
-  handlerClose=(e)=>{
-    if (e.target.nodeName==='DIV' || e.code==='Escape'){
-      this.setState({isModalOpen: 0})
-    }
-  }
-  handleSubmit=(query)=>{
-    this.setState({isQuery: query.trim()})
-    
-  }
-
-  LoadMore=(e)=>{
-    e.preventDefault();
-    e.target.setAttribute('disabled','');
-    this.setState((prev)=>{return {page: prev.page+1}});
-    this.setState({isLoading: true});
-    setTimeout(()=>{
-    try{
-      getImage(this.state.isQuery, this.state.page)
-      .then(({data})=>{
-        const objImg = data.hits.map((el)=>({id: nanoid(), webformatURL: el.webformatURL, largeImageURL: el.largeImageURL}));
-        this.setState((prev)=>{return {objImg: [...prev.objImg,...objImg], isLoading: false}});
-      });
-    }catch(error){
-      console.log(error);
-      this.setState({error});
-    }finally{
-      e.target.removeAttribute('disabled','');
-    }
-  },1000);
-  }
-  render() {
-    return(
-      <>
-        {/* {this.state.isLoading && createPortal(<Loader/>, spinner)} */}
-        {/* {this.state.isModalOpen!==0  && createPortal(<Modal listImg={this.state.objImg} id={this.state.isModalOpen} handlerClose={this.handlerClose}/>, modal)} */}
-        <div className={css.App}>
-          <Searchbar handleSubmit={this.handleSubmit}/>
-          <ImageGallery listImg={this.state.objImg} handlerOpenImg={this.handlerOpenImg}/>
-          {this.state.objImg.length!==0 && (this.state.page * 12 <= this.state.totalPage) && <LoadMore LoadMore={this.LoadMore}/>}
-        </div>
-      </>
-    )
-  }
 }
-export  {App}
+export const App = ()=>{
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(()=>{
+    if(state.isQuery){
+      dispatch({isLoading: true})
+        setTimeout(()=>{
+        try{
+          getImage(state.isQuery, 1)
+          .then(({data})=>{
+            const objImg = data.hits.map((el)=>({id: nanoid(), webformatURL: el.webformatURL, largeImageURL: el.largeImageURL}));
+            dispatch({objImg: {objImg, totalPage: data.totalHits, isLoading: false, page: 1}});
+            
+          });
+        }catch(error){
+          console.log(error);
+          dispatch({error});
+          
+        }
+        
+        },500);
+      }
+  },[state.isQuery]);
+
+  const handleSubmit=(query)=>{
+        dispatch({query});
+      }
+  const handlerOpenImg=(id)=>{
+          dispatch({isModalOpen: id});
+            
+          }
+  const handlerClose=(e)=>{
+                if (e.target.nodeName==='DIV' || e.code==='Escape'){
+                  dispatch({isModalOpen: 0});
+                }
+              }
+  const handlerLoadMore=(e)=>{
+                e.preventDefault();
+                e.target.setAttribute('disabled','');
+                dispatch({page: state.page+1});
+                dispatch({isLoading: true})
+                setTimeout(()=>{
+                  try{
+                    getImage(state.isQuery, state.page)
+                    .then(({data})=>{
+                      const objImg = data.hits.map((el)=>({id: nanoid(), webformatURL: el.webformatURL, largeImageURL: el.largeImageURL}));
+                      dispatch({objImg: {objImg:[...state.objImg, ...objImg], totalPage: data.totalHits, isLoading: false}});
+                      
+                    });
+                  }catch(error){
+                    console.log(error);
+                    dispatch({error});
+                    
+                  }finally{
+                    e.target.removeAttribute('disabled','');
+                  }
+              },500);
+              }
+  return(
+    <>
+      {state.isLoading && createPortal(<Loader/>, spinner)}
+      {state.isModalOpen!==0  && createPortal(<Modal listImg={state.objImg} id={state.isModalOpen} handlerClose={handlerClose}/>, modal)}
+      <div className={css.App}>
+        <Searchbar handleSubmit={handleSubmit}/>
+        <ImageGallery listImg={state.objImg} handlerOpenImg={handlerOpenImg}/>
+        {state.objImg.length!==0 && (state.page * 12 <= state.totalPage) && <LoadMore LoadMore={handlerLoadMore}/>}  
+        
+      </div>
+      
+    </>
+  )
+}
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,6 +8,7 @@ const override = {
   };
 
 export const Loader = ()=>{
+    
     return(
         <CirclesWithBar height="100" width="100" color="#303f9f" wrapperStyle={override} wrapperClass=""
             visible={true} outerCircleColor="" innerCircleColor=""
