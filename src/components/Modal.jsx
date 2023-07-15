c14a2d6213467d599b8bb8844b845e2df4590c0f
Serhii Ozeran <ozeransm@gmail.com>
2023-07-15 09:04:34 +0300

    update Modal and Searchbar

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,16 @@
-import { Component } from 'react'
-import css from '../Styles.module.css'
-class Modal extends Component{
-componentDidMount(){
-  document.addEventListener('keydown',this.props.handlerClose);
+import { useEffect } from 'react';
+import css from '../Styles.module.css';
+
+export const Modal = ({id, listImg, handlerClose})=>{
+  useEffect(()=>{
+    document.addEventListener('keydown',handlerClose);
+    return ()=>document.removeEventListener('keydown',handlerClose); 
+  },[handlerClose])
+  return(
+    <div className={css.Overlay} onClick={handlerClose}>
+        <div className={css.Modal}>
+            <img src={listImg.filter((el)=>el.id===id)[0].largeImageURL} alt="LargeImage" />
+       </div>
+    </div>
+  )
 }
-componentWillUnmount()   {
-    document.removeEventListener('keydown',this.props.handlerClose);
-}
-  render()  {
-    const {id, listImg, handlerClose} = this.props;
-    return(
-        <div className={css.Overlay} onClick={handlerClose}>
-            <div className={css.Modal}>
-                <img src={listImg.filter((el)=>el.id===id)[0].largeImageURL} alt="LargeImage" />
-           </div>
-        </div>
-    )
-  }
-}
-export {Modal}
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,44 +1,37 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from '../Styles.module.css';
- class Searchbar extends Component{
-    state={
-        isQuery:''
-    }
-    onSubmit=(e)=>{
-        e.preventDefault();
-        this.props.handleSubmit(this.state.isQuery)            
-    }
-    onChange=(e)=>{
-        this.setState({isQuery: e.target.value});
-    }
-    onFocus=()=>{
-        this.setState({isQuery: ''});
-    }
-    
-    render(){
-        return(
-        
-            <header className={css.Searchbar}>
-            <form className={css.SearchForm} onSubmit={this.onSubmit}>
-                <button type="submit" className={css["SearchForm-button"]} >
-                <span className={css["SearchForm-button-label"]}>Search</span>
-                </button>
-    
-                <input
-                className={css["SearchForm-input"]}
-                type="text"
-                name="searchQuery"
-                value={this.state.isQuery}
-                onChange={this.onChange}
-                onFocus={this.onFocus}
-                placeholder="Search images and photos"
-                />
-            </form>
-            </header>
-            
-        )
-    }
- }
- export { Searchbar }
-   
+export const Searchbar = ({handleSubmit})=>{
+    const [isQuery, setisQuery] = useState('');
 
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        handleSubmit(isQuery)            
+    }
+    const onChange=(e)=>{
+        setisQuery(e.target.value);
+    }
+    const onFocus=()=>{
+        setisQuery('');
+    }
+    return(
+        
+        <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={onSubmit}>
+            <button type="submit" className={css["SearchForm-button"]} >
+            <span className={css["SearchForm-button-label"]}>Search</span>
+            </button>
+
+            <input
+            className={css["SearchForm-input"]}
+            type="text"
+            name="searchQuery"
+            value={isQuery}
+            onChange={onChange}
+            onFocus={onFocus}
+            placeholder="Search images and photos"
+            />
+        </form>
+        </header>
+        
+    )
+}
